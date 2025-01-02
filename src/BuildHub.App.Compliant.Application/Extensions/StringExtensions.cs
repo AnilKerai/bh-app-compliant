aa28a7e8b6f6ec650086592ccb3f8996783f49a0
Anil Kerai <kerai.anil@googlemail.com>
2025-01-02 21:39:16 +0000

    adding string extension to split camel case

---
+++ src/BuildHub.App.Compliant.Application/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BuildHub.App.Compliant.Application.Extensions;
+
+public static class StringExtensions
+{
+    public static string SplitCamelCase(this string input)
+    {
+        return Regex
+            .Replace(input, "([A-Z])", " $1", RegexOptions.Compiled)
+            .Trim();
+    }
+}
